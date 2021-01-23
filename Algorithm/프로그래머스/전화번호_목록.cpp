eaf3f445fda12544c7ed767688edc58357c4d0f7
Hanturtle <tjfdnjs0829@naver.com>
2021-01-23 18:02:39 +0900

    전화번호 목록 해시 사용하여 해결

--- Algorithm/프로그래머스/전화번호_목록.cpp
+++ Algorithm/프로그래머스/전화번호_목록.cpp
@@ -1,18 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-    sort(phone_book.begin(), phone_book.end());
-
-    for(int i=0;i<phone_book.size()-1;i++){
-        if(phone_book.at(i+1).substr(0,phone_book.front().length()) == phone_book.at(i)){
-            answer = false;
-            break;
+    unordered_map<string, int> hash_map;
+    for(int i=0;i<phone_book.size();i++){
+        hash_map[phone_book[i]] = 1;
+    }
+    
+    for(int i=0;i<phone_book.size();i++){
+        string num = "";
+        for(int j=0;j<phone_book[i].size();j++){
+            num += phone_book[i][j];
+            if(hash_map[num] && num != phone_book[i])
+                return false;
         }
     }
+    
     return answer;
 }
