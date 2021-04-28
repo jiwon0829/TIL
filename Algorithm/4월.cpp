7d9f91497bed340b7750e9c7c0395d6c0c318e3a
Hanturtle <tjfdnjs0829@naver.com>
2021-04-28 21:35:41 +0900

    4월 3번 해결

--- Algorithm/4월.cpp
+++ Algorithm/4월.cpp
@@ -97,15 +97,17 @@ int solution(string s) {
 
 
 //3번
+#include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
+
 long long solution(vector<int> a, vector<vector<int>> edges) {
     long long answer = 0;
-    
     
     //애초에 0을 만들수 없는 경우
     int sum = 0;
@@ -115,26 +117,61 @@ long long solution(vector<int> a, vector<vector<int>> edges) {
     if(sum)
         return -1;
     
-    sort(edges.begin(), edges.end());
-    
-    for(int i=0;i<edges.size();i++){
-        if(a[edges[i][0]] && a[edges[i][1]]){
-            a[edges[i][1]] += a[edges[i][0]];
-            answer += a[edges[i][0]] > 0 ? a[edges[i][0]] : -a[edges[i][0]];
-            a[edges[i][0]] = 0;
-        }
-        
-        
+    //가능한 경우
+    vector<long long> A;
+    for(int i=0;i<a.size();i++){
+        A.push_back((long long)a[i]);
     }
-    for(int j=0;j<a.size();j++){
-        if(a[j])
+    vector<int> cnt(a.size(), 0);
+    vector<vector<int>> tree(a.size(), vector<int>());
+
+    for (int i = 0; i < edges.size(); i++){
+        tree[edges[i][0]].push_back(edges[i][1]);
+        tree[edges[i][1]].push_back(edges[i][0]);
+        
+        //리프노드 판별을 위해
+        cnt[edges[i][0]]++;
+        cnt[edges[i][1]]++;
+    }
+    
+    //리프노드면 부모노드와만 간선이 이루어졌음
+    vector<int> LeafNode;
+    for (int i = 0; i < a.size(); i++){
+        if (cnt[i] == 1)
+            LeafNode.push_back(i);
+    }
+
+    vector<int> check(a.size(), 0); //더했는지 판별
+    queue<int> q;
+    for (int i = 0; i < LeafNode.size(); i++)
+        q.push(LeafNode[i]);
+ 
+    while (q.empty() == false){
+        int now = q.front();
+        check[now] = 1;
+        q.pop();
+ 
+        for (int i = 0; i < tree[now].size(); i++){
+            int next = tree[now][i];
+            if (check[next])
+                continue;
+            A[next] += A[now];
+            answer += abs(A[now]);
+            A[now] = 0;
+            cnt[next]--;
+            if (cnt[next] == 1)
+                q.push(next);
+        }
+    }
+    
+    for(int i=0;i<a.size();i++){
+        if(A[i])
             return -1;
     }
     
-    
-    
     return answer;
 }
+
 */
 
 
