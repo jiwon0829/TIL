5813c756c65547336ad020117f48a5fa5cb3429f
Han jiwon <63095347+jiwon0829@users.noreply.github.com>
2022-06-20 17:01:09 +0900

    Delete chat_server directory

--- chat_server/code/ClientMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "Client.h"
-
-
-int main(int argc, char *argv[]){
-	Client client;
-	client.Start();
-	
-	return 0;
-}
--- chat_server/code/ServerMain.cpp
+++
@@ -1,8 +0,0 @@
-#include "Server.h"
-
-int main(int argc, char *argv[]){
-	Server server;
-	server.Start();
-	
-	return 0;
-}
